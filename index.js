229c87f9583c25f87bc43bbcbb679e08a9129c69
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-03 04:03:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten (name){
 
-  return kittens.push(name); 
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten (name){
+  return kittens.unshift(name);
 }
